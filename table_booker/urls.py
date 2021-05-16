c1e8d4d93f2e2161e4f3c49d9f7ca4ed0a977e87
buthlezi <buthlezi@nwpc-z390-ud.home>
2021-05-16 03:24:14 +0100

    Initial directory setup

---
+++ table_booker/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+# from present directory (table_booker) import views
+# from .. implies a folder outside present folder
+
+app_name = 'table_booker'
+
+urlpatterns = [
+    path('', views.home_page, name='home'),
+    # for a naked request, go into views and find a function called home_page - referenced as 'home'
+    path('login', views.login_page, name='login'),
+    path('logout', views.logout_page, name='logout'),
+    path('signup', views.login_page, name='signup'),
+
+]
