4bee2e281b87651516d9ca00070f5b0eb8973d8b
buthlezi <buthlezi@nwpc-z390-ud.home>
2021-05-29 01:35:44 +0100

    Test login page

--- table_booker/tests.py
+++ table_booker/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 
 import datetime
 
+from django.contrib.auth.forms import AuthenticationForm
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
@@ -11,7 +12,9 @@ from .factories import RestaurantFactory, TableFactory, UserFactory
 from .forms import BookingForm
 from .models import Restaurant
 
+
 class HomePageTests(TestCase):
+    
     def setUp(self):
         self.user = UserFactory()
         self.restaurant = RestaurantFactory()
@@ -35,4 +38,43 @@ class HomePageTests(TestCase):
         # breakpoint()
         
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "home.html")    
+        self.assertTemplateUsed(response, "home.html")  
+             
+class LoginPageTest(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.url = "/login"
+        self.response = self.client.get(self.url)
+        
+    def test_blank_login_page(self):
+        context_form = self.response.context["login_form"]
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertIsInstance(context_form, AuthenticationForm)
+        self.assertTemplateUsed(self.response, "login.html")
+        
+        
+    def test_successful_login(self):
+        data = {
+            "username": self.user.username,
+            "password": "top-secret",
+        }
+        response = self.client.post(self.url, data, follow=True)
+
+        message = list(response.context.get("messages"))[0]
+        self.assertEqual(message.tags, "info")
+        self.assertTrue(
+            f"You are now logged in as {self.user.username}." in message.message
+        )
+        self.assertRedirects(response, "/", status_code=302)
+        
+    def test_unsuccessful_login(self):
+        data = {
+            "username": self.user.username,
+            "password": "wrong-password",
+        }
+        response = self.client.post(self.url, data, follow=True)
+
+        message = list(response.context.get("messages"))[0]
+        self.assertEqual(message.tags, "error")
+        self.assertTrue("Invalid username or password." in message.message)
